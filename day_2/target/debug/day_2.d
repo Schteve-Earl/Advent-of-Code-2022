f4056d2a65b4c2114e931eb05ed438323b889861
Stephen Earl <searl0030@gmail.com>
2022-12-02 08:29:03 -0700

    Really adding .gitignore and removing target, now that it's not 11:30 at night

--- day_2/target/debug/day_2.d
+++
@@ -1,1 +0,0 @@
-/home/stephen/Advent-of-Code-2022/day_2/target/debug/day_2: /home/stephen/Advent-of-Code-2022/day_2/src/main.rs
--- day_2/target/debug/deps/day_2-ad3c106ef6cb499b.d
+++
@@ -1,5 +0,0 @@
-/home/stephen/Advent-of-Code-2022/day_2/target/debug/deps/day_2-ad3c106ef6cb499b: src/main.rs
-
-/home/stephen/Advent-of-Code-2022/day_2/target/debug/deps/day_2-ad3c106ef6cb499b.d: src/main.rs
-
-src/main.rs:
