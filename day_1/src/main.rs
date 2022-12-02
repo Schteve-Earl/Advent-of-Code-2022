d36476c2e259004585d60ad7c07d9366d74703fb
Stephen Earl <searl0030@gmail.com>
2022-12-02 09:42:21 -0700

    Optimized day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,69 +1,38 @@
 
 #![allow(dead_code)]
 
-pub mod day_1 {
-    use std::collections::HashMap;
-    struct Elf {
-        total: u32,
-        calories: Vec<u32>,
-    }
-
-    impl Elf {
-        fn sum_calories(&mut self) {
-            self.total = self.calories.iter().sum();
-        }
-
-        fn add_calories(&mut self, calorie: u32) {
-            self.calories.push(calorie);
-        }
-
-        fn get_calories(&mut self) -> u32 {
-            self.total
-        }
-    }
-    
-    pub fn compute_1_calories()
+mod day_1 {
+    pub fn compute_calories(size: usize)
     {
-        let mut elves: HashMap<u32, Elf> = HashMap::new();
-        let mut index: u32 = 0;
+        let mut r = 0;
+        let mut vec: Vec<u32> = Vec::new();
         
-        let problem_input = include_str!("../input.txt")
+        include_str!("../input.txt")
                             .to_string()
                             .lines()
                             .map(|n| n.to_string())
-                            .collect::<Vec<String>>();
+                            .collect::<Vec<String>>().iter().for_each(|c| 
+                            { 
+                                if !c.is_empty() {
+                                    r += c.parse::<u32>().unwrap();
+                                }
+                                else {
+                                    vec.push(r);
+                                    r = 0;
+                                }
+                            });
 
-        let mut most_calories: Vec<u32> = vec![0; 3];
-        elves.insert(index, Elf { total: 0, calories: Vec::new() });
+        vec.sort();
+        vec.reverse();
+        vec.resize(size, 0);
+        let result: u32 = vec.iter().sum();
 
-        for p in problem_input {
-            if p.is_empty() {
-                elves.entry(index).and_modify(| elf | { elf.sum_calories()});
-                let mut calories: u32 = 0;
-                elves.entry(index).and_modify(| elf | { calories = elf.get_calories()});
-
-                for i in 0..most_calories.len() {
-                    if most_calories[i] < calories {
-
-                        most_calories.push(calories);
-                        most_calories.sort();
-                        most_calories.remove(0);
-                        break;
-                    }
-                }
-                index += 1;
-                elves.insert(index, Elf { total: 0, calories: Vec::new() });
-                continue;
-            }
-            elves.entry(index).and_modify(| elf | { elf.add_calories(p.parse().unwrap())});
-        }
-
-        let mc: u32 = most_calories.iter().sum();
-        println!("{mc}");
+        println!("Top {size} calories is {result}");
     }
 
 }
 
 fn main() {
-    crate::day_1::compute_1_calories();
+    crate::day_1::compute_calories(1);
+    crate::day_1::compute_calories(3);
 }
